e9784a19825d9b5c2ea339b438699cf52c5c9819
Mayank Vikram <152918733+mayankv45@users.noreply.github.com>
2026-02-12 00:24:12 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: "/pfa-mahotsav-web/",
   server: {
     host: "::",
     port: 8080,
